70c10fa2368a78944951f57a03b2cade761c2da6
Barbara Lopez <barbara.nyd@gmail.com>
2026-05-08 11:24:19 -0400

    primer commit

---
+++ Untitled-1.js
@@ -0,0 +1,40 @@
+let nombre = "Bárbara";
+let apellido = "López";
+
+console.log(nombre, apellido);
+
+let comidaFavorita = "Pastas";
+console.log(comidaFavorita);
+
+let = a = 10;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a % b);
+
+let edad = 15;
+console.log(edad > 10);
+console.log(edad < 18);
+
+let numero = 8;
+console.log(numero > 5 && numero < 10);
+
+let nota = 6
+console.log(nota == 7)
+
+let temperatura = 25;
+console.log(temperatura >= 30);
+
+let numero1 = 20;
+let numero2 = 15;
+console.log(numero1 == numero2); 
+console.log(numero1 >= numero2);
+
+let edad2 = 17;
+let tienePermiso = true;
+console.log(edad2 >= 18 || tienePermiso);
+
+let usuario = "admin";
+let password = "1234";
+console.log(usuario == "admin" && password == "1234");
